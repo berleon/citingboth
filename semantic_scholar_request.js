ba9017161574761486d109855f17de312d70bb72
berleon <github@leon-sixt.de>
2020-10-06 22:35:06 +0200

    add skeleton

--- semantic_scholar_request.js
+++ semantic_scholar_request.js
@@ -4,10 +4,18 @@ function getPaper(name) {
  fetch('https://api.semanticscholar.org/v1/paper/URL:https://www.semanticscholar.org/paper/Fabricating-genetically-engineered-high-power-using-Lee-Yi/6a84672aa6a3ee40ab3e14a7f3007e23255f16be') .then(response => response.json())
   .then(data => console.log(data));
 }
-function getPapers() {
+
+function getPaperByIdApi(paperId) {
+    return fetch('https://api.semanticscholar.org/v1/paper/' + paperId) .then(response => response.json())
+}
+
+function renderPapersCitingBoth() {
   let url1 = document.getElementById("url1");
   let url2 = document.getElementById("url2");
   let result_list = document.getElementById("result_list");
+  let result_description = document.getElementById("result_description");
+  let load_spinner = document.getElementById("load-spinner");
+  load_spinner.setAttribute('style', '');
   let prefix = 'https://api.semanticscholar.org/v1/paper/URL:';
 
   let json_prom1 = fetch(prefix + url1.value).then(response => response.json());
@@ -22,16 +30,56 @@ function getPapers() {
       let refIntersect = refsIds[0].filter(ref => refsIds[1].includes(ref));
       console.log(refIntersect);
 
+
+      result_description.innerHTML = `<b>${papers[0]['title']}</b> and <b>${papers[1]['title']}</b> are cited by <b>${refIntersect.length}</b> papers:`;
+
       result_list.textContent = '';
       refIntersect.map(refId => {
         let ref = id2ref[refId];
         console.log(ref);
         var li = document.createElement('li');
-        elem = '<a href=' + ref['url'] + '>' + ref['title'] + '</a><br>by ';
+        elem = '<a href=' + ref['url'] + '>' + ref['title'] + '</a><br> ';
         elem += ref['authors'].map(author => author['name']).join(", ");
+        elem += ". In " + ref['venue'];
+        elem += "  (" + ref['year'] + ")";
         li.innerHTML = elem
         result_list.appendChild(li);
+        load_spinner.setAttribute('style', 'display:none');
       });
   }
   );
+}
+function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function renderAuthorPapersToHTML() {
+  let author_id = document.getElementById("author_id");
+  let result_list = document.getElementById("result_list");
+  let prefix = 'https://api.semanticscholar.org/v1/author/';
+
+  result_list.textContent = '';
+  fetch(prefix + author_id.value).then(response => response.json()).then(data => {
+      // console.log(data);
+      let paper_promises = data['papers'].map((paper, idx) => {
+          // console.log(paper);
+          return sleep(idx*500).then(() => getPaperByIdApi(paper['paperId']))
+          //getPaperByIdApi(paper['paperId'])
+      });
+      Promise.allSettled(paper_promises).then(promises => {
+          let papers = promises.map(elem => elem['value'])
+          console.log(papers)
+          papers.sort((p1, p2) => p1['year'] < p2['year']);
+          papers.map(paper => {
+            console.log(paper);
+            var li = document.createElement('li');
+            elem = '<a href=' + paper['url'] + '>' + paper['title'] + '</a><br>';
+            elem += paper['authors'].map(author => author['name']).join(", ") + ".";
+            elem += " In " + paper['venue'];
+            elem += ", " + paper['year'];
+            li.innerHTML = elem
+            result_list.appendChild(li);
+          });
+      });
+  });
 }
